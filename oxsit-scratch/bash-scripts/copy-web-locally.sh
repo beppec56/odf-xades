536747dcccbc16480d982448317788952c973794
beppec56 <beppec56@ae2944e6-8456-422b-b884-7dc9435bd57f>
2009-04-08 13:36:48 +0000

    Updated script file to new directory structure.
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@127 ae2944e6-8456-422b-b884-7dc9435bd57f

--- oxsit-scratch/bash-scripts/copy-web-locally.sh
+++ oxsit-scratch/bash-scripts/copy-web-locally.sh
@@ -6,10 +6,16 @@
 # from the local web server can be used as a src to copy to publishing web
 # 
 # usage: from inside the root directory in web subproject, run:
-# ../scratch/bash-scripts/copy-web-locally.sh
+# ../oxsit-scratch/bash-scripts/copy-web-locally.sh
 WEBLOCAL="/var/www/test-osor"
 cp -apuvf . $WEBLOCAL
 #cd /var/www/test-osor
 #remove the un-needed .svn files...
 find $WEBLOCAL -name ".svn" -type d -exec rm -rf '{}' \;
 rm $WEBLOCAL"/README"
+rm $WEBLOCAL"/css/blueprint/AUTHORS.textile"
+rm $WEBLOCAL"/css/blueprint/README.textile"
+rm $WEBLOCAL"/css/blueprint/plugins/buttons/readme.txt"
+rm $WEBLOCAL"/css/blueprint/plugins/fancy-type/readme.txt"
+rm $WEBLOCAL"/css/blueprint/plugins/link-icons/readme.txt"
+rm $WEBLOCAL"/css/blueprint/plugins/rtl/readme.txt"
