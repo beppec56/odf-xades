fb66d73539b84032a39ea611e83a008d750fa841
beppec56 <beppec56@ae2944e6-8456-422b-b884-7dc9435bd57f>
2009-04-16 13:44:14 +0000

    Added new interface type for UNO, plus compiler instruction to build the new registration in OOO.
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@173 ae2944e6-8456-422b-b884-7dc9435bd57f

---
+++ oxsit-sig_opt_uno/idl/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# for Linux, to be used when generation of classes is needed
+#needs OOO sdk in place and initialized
+HOME_OO_SDK="/home/beppe/openoffice.org_sdk"
+SOURCE_SHELL=$HOME_OO_SDK/dsklnx
+
+. $SOURCE_SHELL/setsdkenv_unix.sh
+# compile file
+idlc -I $OO_SDK_HOME/idl OXDocumentSignatures.idl
+
+#remove old file
+rm ../rdb/oxsit_types.rdb
+
+#merge the registry file
+regmerge -v ../rdb/oxsit_types.rdb /UCR OXDocumentSignatures.urd
+
+#prepare the java classes
+javamaker -BUCR -Tit.plio.ext.oxsit.ooo.cert.OXDocumentSignatures -nD $OO_SDK_URE_HOME/share/misc/types.rdb ../rdb/oxsit_types.rdb
+
+#prepare the java classes
+cppumaker -BUCR -Tit.plio.ext.oxsit.ooo.cert.OXDocumentSignatures $OO_SDK_URE_HOME/share/misc/types.rdb ../rdb/oxsit_types.rdb
+ 
