1b66e797e74e0303fb38373ab93052f0385d21d9
beppec56 <beppec56@ae2944e6-8456-422b-b884-7dc9435bd57f>
2009-04-18 20:26:12 +0000

    Add new UNO type Java classes for new uno interfaces/services
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@182 ae2944e6-8456-422b-b884-7dc9435bd57f

---
+++ oxsit-uno_types/idl/compile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# for Linux only, to be used when generation of interface class is needed
+# not normally needed when the Interface is defined
+# for Windows some fixes may be necesary
+# it needs OOO sdk in place and initialized
+#
+HOME_OO_SDK="/home/beppe/openoffice.org_sdk"
+SOURCE_SHELL=$HOME_OO_SDK/dsklnx
+
+#HOME_OO_SDK="/home/beppe/openoffice.org2.2_sdk"
+#SOURCE_SHELL=$HOME_OO_SDK/
+
+CLASS_FILE=XOXDocumentSignatures
+SERVICE_FILE=DocumentSignatures
+
+echo "source env for OOo SDK"
+. $SOURCE_SHELL/setsdkenv_unix.sh
+
+
+echo "compile idl file"
+idlc -I $OO_SDK_HOME/idl $CLASS_FILE".idl"
+
+echo "remove old registry file"
+rm ../rdb/oxsit-uno_types.uno.rdb
+
+echo "merge the registry file"
+regmerge -v ../rdb/oxsit-uno_types.uno.rdb /UCR $CLASS_FILE".urd"
+
+echo "prepare the java classes"
+javamaker -BUCR -O../pre-built-type-classes -T"it.plio.ext.oxsit.ooo.cert."$CLASS_FILE -nD $OO_SDK_URE_HOME/share/misc/types.rdb ../rdb/oxsit-uno_types.uno.rdb
