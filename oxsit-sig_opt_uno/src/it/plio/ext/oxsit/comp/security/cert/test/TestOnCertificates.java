c943d85193a35aea77c2bbedf0fb76f2b0cd37c3
resoli <resoli@ae2944e6-8456-422b-b884-7dc9435bd57f>
2009-06-22 13:24:51 +0000

    First attempt to support virtual slots
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@452 ae2944e6-8456-422b-b884-7dc9435bd57f

--- oxsit-sig_opt_uno/src/it/plio/ext/oxsit/comp/security/cert/test/TestOnCertificates.java
+++ oxsit-sig_opt_uno/src/it/plio/ext/oxsit/comp/security/cert/test/TestOnCertificates.java
@@ -71,14 +71,13 @@ import com.sun.star.uno.XComponentContext;
  */
 public class TestOnCertificates {
 
-	
 	protected DynamicLogger m_aLogger;
-	
+
 	protected XComponentContext m_xCC;
 
 	public TestOnCertificates(XComponentContext _CTX) {
 		m_xCC = _CTX;
-		m_aLogger = new DynamicLogger(this,_CTX);
+		m_aLogger = new DynamicLogger(this, _CTX);
 		m_aLogger.enableLogging();
 		m_aLogger.ctor();
 	}
@@ -113,12 +112,12 @@ public class TestOnCertificates {
 		public CertInfo(X509Certificate x) {
 			certName = toCNNames("" + x.getSubjectDN());
 			c = x;
-//			org.bouncycastle.asn1.ASN1Encodable ab;
-//			ASN1Sequence aSeq =  ASN1Object.fromByteArray();
+			//			org.bouncycastle.asn1.ASN1Encodable ab;
+			//			ASN1Sequence aSeq =  ASN1Object.fromByteArray();
 			try {
 				byte[] derdata = c.getEncoded();//c.getTBSCertificate();
-//
-				ByteArrayInputStream as = new ByteArrayInputStream(derdata); 
+				//
+				ByteArrayInputStream as = new ByteArrayInputStream(derdata);
 				ASN1InputStream aderin = new ASN1InputStream(as);
 				DERObject ado = aderin.readObject();
 				xc509 = new X509CertificateStructure((ASN1Sequence) ado);
@@ -146,94 +145,92 @@ public class TestOnCertificates {
 			Vector<DERObjectIdentifier> oidv = _aName.getOIDs();
 			Vector<?> values = _aName.getValues();
 			HashMap<DERObjectIdentifier, String> hm = new HashMap<DERObjectIdentifier, String>(20);
-			for(int i=0; i< oidv.size(); i++) {
-//				m_aLoggerDialog.info(oidv.elementAt(i).getId()+" = "+values.elementAt(i)+" "+X509Name.DefaultSymbols.get(oidv.elementAt(i)));
+			for (int i = 0; i < oidv.size(); i++) {
+				//				m_aLoggerDialog.info(oidv.elementAt(i).getId()+" = "+values.elementAt(i)+" "+X509Name.DefaultSymbols.get(oidv.elementAt(i)));
 				hm.put(oidv.elementAt(i), values.elementAt(i).toString());
 			}
 			//look for givename (=nome di battesimo)
 			{
 				String nome = "<no name>";
 				//see BC source code for details about DefaultLookUp behaviour
-				DERObjectIdentifier oix = (DERObjectIdentifier)(X509Name.DefaultLookUp.get("givenname")); 
-				if(hm.containsKey(oix)) {
+				DERObjectIdentifier oix = (DERObjectIdentifier) (X509Name.DefaultLookUp.get("givenname"));
+				if (hm.containsKey(oix)) {
 					nome = hm.get(oix).toString();
-					oix = (DERObjectIdentifier)(X509Name.DefaultLookUp.get("surname"));
-					if(hm.containsKey(oix))
-						nome = nome +" "+hm.get(oix).toString();
-				}
-				else {
+					oix = (DERObjectIdentifier) (X509Name.DefaultLookUp.get("surname"));
+					if (hm.containsKey(oix))
+						nome = nome + " " + hm.get(oix).toString();
+				} else {
 					//check for CN
-					oix = (DERObjectIdentifier)(X509Name.DefaultLookUp.get("cn")); 
-					if(hm.containsKey(oix)) {
+					oix = (DERObjectIdentifier) (X509Name.DefaultLookUp.get("cn"));
+					if (hm.containsKey(oix)) {
 						nome = hm.get(oix).toString();
-					}
-					else {
+					} else {
 						//if still not, check for pseudodym
-						oix = (DERObjectIdentifier)(X509Name.DefaultLookUp.get("pseudonym"));
-						if(hm.containsKey(oix))
-							nome = hm.get(oix).toString();						
+						oix = (DERObjectIdentifier) (X509Name.DefaultLookUp.get("pseudonym"));
+						if (hm.containsKey(oix))
+							nome = hm.get(oix).toString();
 					}
 				}
 				m_aLogger.info(nome);
-			}			
+			}
 		}
 
-		protected void printX509Name(X509Name _aName) {			
-//print the subject
+		protected void printX509Name(X509Name _aName) {
+			//print the subject
 			//order of printing is as got in the CNIPA spec
 			//first, grab the OID in the subject name
-			Vector<DERObjectIdentifier> oidv =  _aName.getOIDs();
+			Vector<DERObjectIdentifier> oidv = _aName.getOIDs();
 			Vector<?> values = _aName.getValues();
 			HashMap<DERObjectIdentifier, String> hm = new HashMap<DERObjectIdentifier, String>(20);
 			String sAname = "\r\n";
-			for(int i=0; i< oidv.size(); i++) {
-				sAname = sAname + X509Name.DefaultSymbols.get(oidv.elementAt(i))+"="+values.elementAt(i).toString()+
-						" (OID: "+oidv.elementAt(i).toString()+") \r\n";
+			for (int i = 0; i < oidv.size(); i++) {
+				sAname = sAname + X509Name.DefaultSymbols.get(oidv.elementAt(i)) + "=" + values.elementAt(i).toString()
+						+ " (OID: " + oidv.elementAt(i).toString() + ") \r\n";
 				hm.put(oidv.elementAt(i), values.elementAt(i).toString());
 			}
 			m_aLogger.info(sAname);
 		}
-		
+
 		protected String printCertDate(Date _aTime) {
 			Calendar calendar = new GregorianCalendar();
-			calendar.setTime(_aTime);	
-	//string with time only
+			calendar.setTime(_aTime);
+			//string with time only
 			String time = String.format("%1$tb %1$td %1$tY %1$tH:%1$tM:%1$tS (%1$tZ)", calendar);
 			return time;
 		}
 
 		public String printCert() {
-			
+
 			printSubjectNameForNode(xc509.getSubject());
-			m_aLogger.info("Version: V"+c.getVersion());
-			m_aLogger.info("Serial number: "+xc509.getSerialNumber().getValue());
+			m_aLogger.info("Version: V" + c.getVersion());
+			m_aLogger.info("Serial number: " + xc509.getSerialNumber().getValue());
 			printX509Name(xc509.getIssuer());
-			m_aLogger.info("Valid not before: "+printCertDate(xc509.getStartDate().getDate()));
-			m_aLogger.info("Valid not after:  "+printCertDate(xc509.getEndDate().getDate()));
+			m_aLogger.info("Valid not before: " + printCertDate(xc509.getStartDate().getDate()));
+			m_aLogger.info("Valid not after:  " + printCertDate(xc509.getEndDate().getDate()));
 			printX509Name(xc509.getSubject());
 
 			AlgorithmIdentifier aid = xc509.getSignatureAlgorithm();
 			DERObjectIdentifier oi = aid.getObjectId();
 
-			m_aLogger.info("Subject Public Signature Algorithm: "+((
-					xc509.getSubjectPublicKeyInfo().getAlgorithmId().getObjectId().equals(X509CertificateStructure.rsaEncryption)) ?
-							"pkcs-1 rsaEncryption" : oi.getId()
-							));
+			m_aLogger.info("Subject Public Signature Algorithm: "
+					+ ((xc509.getSubjectPublicKeyInfo().getAlgorithmId().getObjectId()
+							.equals(X509CertificateStructure.rsaEncryption)) ? "pkcs-1 rsaEncryption" : oi.getId()));
 
 			byte[] sbjkd = xc509.getSubjectPublicKeyInfo().getPublicKeyData().getBytes();
 
-			m_aLogger.info("Subject Public Key Data:\n"+Helpers.printHexBytes(sbjkd));
+			m_aLogger.info("Subject Public Key Data:\n" + Helpers.printHexBytes(sbjkd));
 
-			m_aLogger.info("Signature Algorithm: "+((
-					xc509.getSignatureAlgorithm().getObjectId().equals(X509CertificateStructure.sha1WithRSAEncryption)) ? 
-							"pkcs-1 sha1WithRSAEncryption" : oi.getId()));
+			m_aLogger
+					.info("Signature Algorithm: "
+							+ ((xc509.getSignatureAlgorithm().getObjectId().equals(X509CertificateStructure.sha1WithRSAEncryption)) ? "pkcs-1 sha1WithRSAEncryption"
+									: oi.getId()));
 
-/*			sbjkd = xc509.getSignature().getBytes();
-			m_aLoggerDialog.info("Signature Data:\n"+Helpers.printHexBytes(sbjkd));*/
+			/*			sbjkd = xc509.getSignature().getBytes();
+						m_aLoggerDialog.info("Signature Data:\n"+Helpers.printHexBytes(sbjkd));*/
 
 			//obtain a byte block of the entire certificate data
-			ByteArrayOutputStream   bOut = new ByteArrayOutputStream();
-			DEROutputStream         dOut = new DEROutputStream(bOut);
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			DEROutputStream dOut = new DEROutputStream(bOut);
 			try {
 				dOut.writeObject(xc509);
 				byte[] certBlock = bOut.toByteArray();
@@ -243,59 +240,59 @@ public class TestOnCertificates {
 				digsha1.update(certBlock, 0, certBlock.length);
 				byte[] hashsha1 = new byte[digsha1.getDigestSize()];
 				digsha1.doFinal(hashsha1, 0);
-				m_aLogger.info("Certificate SHA1 Thumbprint: "+Helpers.printHexBytes(hashsha1));
-				MD5Digest  digmd5 = new MD5Digest();
+				m_aLogger.info("Certificate SHA1 Thumbprint: " + Helpers.printHexBytes(hashsha1));
+				MD5Digest digmd5 = new MD5Digest();
 				digmd5.update(certBlock, 0, certBlock.length);
 				byte[] hashmd5 = new byte[digmd5.getDigestSize()];
 				digmd5.doFinal(hashmd5, 0);
-				m_aLogger.info("Certificate MD5 Thumbprint: "+Helpers.printHexBytes(hashmd5));
+				m_aLogger.info("Certificate MD5 Thumbprint: " + Helpers.printHexBytes(hashmd5));
 
 			} catch (IOException e) {
 				e.printStackTrace();
 				m_aLogger.severe("certif data print!", e);
 			}
-			
+
 			//print extensions
 			X509Extensions xc509Ext = xc509.getTBSCertificate().getExtensions();
-			
+
 			Vector<DERObjectIdentifier> extoid = new Vector();
-			for(Enumeration<DERObjectIdentifier> enume = xc509Ext.oids(); enume.hasMoreElements();) {
+			for (Enumeration<DERObjectIdentifier> enume = xc509Ext.oids(); enume.hasMoreElements();) {
 				extoid.add(enume.nextElement());
-			}			
-			CertificateExtensionDisplayHelper aHelper = new CertificateExtensionDisplayHelper(m_xCC,new Locale("it"), "", "",
-					true, new DynamicLoggerDialog(this,m_xCC));
+			}
+			CertificateExtensionDisplayHelper aHelper = new CertificateExtensionDisplayHelper(m_xCC, new Locale("it"), "", "",
+					true, new DynamicLoggerDialog(this, m_xCC));
 			MessageConfigurationAccess m_aRegAcc = null;
-			m_aRegAcc = new MessageConfigurationAccess(m_xCC, m_xCC.getServiceManager() );
-			
+			m_aRegAcc = new MessageConfigurationAccess(m_xCC, m_xCC.getServiceManager());
+
 			//first the critical one
 			m_aLogger.info("Critical Extensions:");
-			for(int i=0; i<extoid.size();i++) {
+			for (int i = 0; i < extoid.size(); i++) {
 				X509Extension aext = xc509Ext.getExtension(extoid.get(i));
-				if(aext.isCritical())
+				if (aext.isCritical())
 					try {
-						m_aLogger.log(extoid.get(i).getId()+ " "+m_aRegAcc.getStringFromRegistry(extoid.get(i).getId())+term+
-								aHelper.examineExtension(aext, extoid.get(i), null));
+						m_aLogger.log(extoid.get(i).getId() + " " + m_aRegAcc.getStringFromRegistry(extoid.get(i).getId()) + term
+								+ aHelper.examineExtension(aext, extoid.get(i), null));
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 			}
-			
+
 			m_aLogger.info("Non Critical Extensions:");
-			for(int i=0; i<extoid.size();i++) {
+			for (int i = 0; i < extoid.size(); i++) {
 				X509Extension aext = xc509Ext.getExtension(extoid.get(i));
-				if(!aext.isCritical())
+				if (!aext.isCritical())
 					try {
-						m_aLogger.log(extoid.get(i).getId()+ " "+m_aRegAcc.getStringFromRegistry(extoid.get(i).getId())+term+
-								aHelper.examineExtension(aext, extoid.get(i), null));
+						m_aLogger.log(extoid.get(i).getId() + " " + m_aRegAcc.getStringFromRegistry(extoid.get(i).getId()) + term
+								+ aHelper.examineExtension(aext, extoid.get(i), null));
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 			}
-			
+
 			m_aRegAcc.dispose();
-			
+
 			//print the certificate path
 			printSubjectNameForNode(xc509.getIssuer());
 			return "";
@@ -333,7 +330,7 @@ public class TestOnCertificates {
 		// TODO Auto-generated method stub
 		m_aLogger.entering("testMethod");
 
-		PCSCHelper pcsc = new PCSCHelper(null,null, true, null, m_aLogger);
+		PCSCHelper pcsc = new PCSCHelper(null, null, true, null, m_aLogger);
 
 		m_aLogger.log("After 'new PCSCHelper'");
 
@@ -351,44 +348,53 @@ public class TestOnCertificates {
 			String currReader = cIr.getReader();
 
 			ci = cIr.getCard();
-			
+
 			if (ci != null) {
-				
+
 				m_aLogger.log("Informations found for this card:");
-				m_aLogger.log("\tDescription:\t"
-						+ ci.m_sDescription);
-				m_aLogger.log("\tManufacturer:\t"
-						+ ci.m_sManufacturer);
+				m_aLogger.log("\tDescription:\t" + ci.m_sDescription);
+				m_aLogger.log("\tManufacturer:\t" + ci.m_sManufacturer);
 				m_aLogger.log("\tATR:\t\t" + ci.getATRCode());
 				m_aLogger.log("\tCriptoki:\t" + ci.getOsLib());
 
 				m_aLogger.log("\tLettura certificati");
-				
+
 				ReadCerts rt = new ReadCerts(null, m_aLogger, currReader, cIr);
-				Collection<?> certsOnToken = rt.getCertsOnToken();
-				if (certsOnToken != null) {
-					Iterator<?> certIt = certsOnToken.iterator();
-					if (certsOnToken.isEmpty()) {
-						m_aLogger.log("\tcertsOnToken vuoto");
-						CertInfo c = new CertInfo();
-						c.setName("Carta presente ma vuota");
 
+				long[] availableToken = rt.getTokens();
+
+				if (availableToken != null) {
+					for (int i = 0; i < availableToken.length; i++) {
+
+						Collection<?> certsOnToken = rt.getCertsOnToken(i);
+						if (certsOnToken != null) {
+							Iterator<?> certIt = certsOnToken.iterator();
+							if (certsOnToken.isEmpty()) {
+								m_aLogger.log("\tcertsOnToken vuoto");
+								CertInfo c = new CertInfo();
+								c.setName("Carta presente ma vuota");
+
+							}
+							while (certIt.hasNext()) {
+
+								m_aLogger
+										.log("******************************************  &&  ****************  &&  *********************************************************");
+								X509Certificate cert = (X509Certificate) certIt.next();
+
+								CertInfo c = new CertInfo(cert);
+								m_aLogger.log(c.printCert());
+								m_aLogger
+										.log("*******************************************************************************************************************");
+
+							}
+
+
+						}
 					}
-					while (certIt.hasNext()) {
-
-						m_aLogger.log("******************************************  &&  ****************  &&  *********************************************************");
-						X509Certificate cert = (X509Certificate) certIt.next();
-						
-						CertInfo c = new CertInfo(cert);
-						m_aLogger.log(c.printCert());
-						m_aLogger.log("*******************************************************************************************************************");
-
-					}
-
-					rt.closeSession();
-					rt.libFinalize();
-					indexToken++;
 				}
+				rt.closeSession();
+				rt.libFinalize();
+				indexToken++;
 
 			} else {
 				m_aLogger.log("No card in reader '" + currReader + "'!");
@@ -396,7 +402,7 @@ public class TestOnCertificates {
 			}
 
 			indexReader++;
-		}		
+		}
 	}
 
 }
