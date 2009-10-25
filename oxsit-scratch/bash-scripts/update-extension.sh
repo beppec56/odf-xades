713ab339aba5d77c29c58fcc2a0521097dbeb6a3
beppec56 <beppec56@ae2944e6-8456-422b-b884-7dc9435bd57f>
2009-10-25 18:43:18 +0000

    Added a bash script to update the extension on GNU/Linux
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@458 ae2944e6-8456-422b-b884-7dc9435bd57f

---
+++ oxsit-scratch/bash-scripts/update-extension.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this file can be used in GNU/Linux to update the extension in OOo
+#
+# IMPORTANT: OOo should be closed before running the script
+#
+cd /opt/openoffice.org3/program
+./unopkg remove -v it.plio.ext.oxsit
+sleep 2
+./unopkg add -v /home/beppe/oxsit.oxt
+#
