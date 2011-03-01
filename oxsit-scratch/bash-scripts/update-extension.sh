64785386142d93b2e70a9c87e7fb9f9dab159835
beppec56 <beppec56@ae2944e6-8456-422b-b884-7dc9435bd57f>
2011-03-01 16:57:23 +0000

    Updated extension internal name in helper installation script.
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@490 ae2944e6-8456-422b-b884-7dc9435bd57f

--- oxsit-scratch/bash-scripts/update-extension.sh
+++ oxsit-scratch/bash-scripts/update-extension.sh
@@ -4,7 +4,7 @@
 # IMPORTANT: OOo should be closed before running the script
 #
 # cd /opt/openoffice.org3/program
-./unopkg remove --shared -v it.plio.ext.oxsit
+./unopkg remove --shared -v com.yacme.ext.oxsit
 sleep 2
 ./unopkg add --shared -v $HOME/oxsit.oxt
 #
