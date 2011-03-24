82373ebe295c15fd1d74f5b8a792767e3c1afca1
beppec56 <beppec56@ae2944e6-8456-422b-b884-7dc9435bd57f>
2011-03-24 08:29:23 +0000

    Added alternative choice of license.
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@512 ae2944e6-8456-422b-b884-7dc9435bd57f

--- oxsit-custom_it/src/com/yacme/ext/oxsit/cust_it/comp/security/cert/CertificateRevocation_IT.java
+++ oxsit-custom_it/src/com/yacme/ext/oxsit/cust_it/comp/security/cert/CertificateRevocation_IT.java
@@ -1,29 +1,45 @@
-/*************************************************************************
- *
- *  Copyright 2009 by Giuseppe Castagno beppec56@openoffice.org
+/* ***** BEGIN LICENSE BLOCK ********************************************
+ * Version: EUPL 1.1/GPL 3.0
  * 
- *  Part of the code is adapted from j4sign, hence part of
- *  the copyright is:
- *	j4sign - an open, multi-platform digital signature solution
- *	Copyright (c) 2004 Roberto Resoli - Servizio Sistema Informativo - Comune di Trento.
+ * The contents of this file are subject to the EUPL, Version 1.1 or 
+ * - as soon they will be approved by the European Commission - 
+ * subsequent versions of the EUPL (the "Licence");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.osor.eu/eupl/european-union-public-licence-eupl-v.1.1
  *
- *  The Contents of this file are made available subject to
- *  the terms of European Union Public License (EUPL) version 1.1
- *  as published by the European Community.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the EUPL.
+ * The Original Code is /oxsit-custom_it/src/com/yacme/ext/oxsit/cust_it/comp/security/cert/CertificateRevocation_IT.java.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  EUPL for more details.
+ * The Initial Developers of the Original Code are
+ * Giuseppe Castagno giuseppe.castagno@acca-esse.it
+ * Roberto Resoli - Servizio Sistema Informativo - Comune di Trento.
+ * 
+ * Portions created by the Initial Developer are Copyright (C) 2009-2011
+ * the Initial Developer. All Rights Reserved.
+ * Part of the code is adapted from j4sign, hence part of
+ * the copyright is:
+ * j4sign - an open, multi-platform digital signature solution
+ * Copyright (c) 2004 Roberto Resoli - Servizio Sistema Informativo - Comune di Trento.
+ * 
+ * Contributor(s):
  *
- *  You should have received a copy of the EUPL along with this
- *  program.  If not, see:
- *  https://www.osor.eu/eupl, http://ec.europa.eu/idabc/eupl.
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 3 or later (the "GPL")
+ * in which case the provisions of the GPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the GPL, and not to allow others to
+ * use your version of this file under the terms of the EUPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EUPL, or the GPL.
  *
- ************************************************************************/
+ * ***** END LICENSE BLOCK ******************************************** */
 
 package com.yacme.ext.oxsit.cust_it.comp.security.cert;
 
--- oxsit-custom_it/src/com/yacme/ext/oxsit/cust_it/security/crl/OCSPQuery.java
+++ oxsit-custom_it/src/com/yacme/ext/oxsit/cust_it/security/crl/OCSPQuery.java
@@ -1,26 +1,41 @@
-/*************************************************************************
+/* ***** BEGIN LICENSE BLOCK ********************************************
+ * Version: EUPL 1.1/GPL 3.0
+ * 
+ * The contents of this file are subject to the EUPL, Version 1.1 or 
+ * - as soon they will be approved by the European Commission - 
+ * subsequent versions of the EUPL (the "Licence");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.osor.eu/eupl/european-union-public-licence-eupl-v.1.1
  *
- *  Copyright 2009 by Giuseppe Castagno beppec56@openoffice.org
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  This code is adapted from www.openscdp.org
+ * The Original Code is /oxsit-custom_it/src/com/yacme/ext/oxsit/cust_it/security/crl/OCSPQuery.java.
  *
- *  The Contents of this file are made available subject to
- *  the terms of European Union Public License (EUPL) version 1.1
- *  as published by the European Community.
+ * The Initial Developer of the Original Code is
+ * Giuseppe Castagno giuseppe.castagno@acca-esse.it
+ * 
+ * Portions created by the Initial Developer are Copyright (C) 2009-2011
+ * the Initial Developer. All Rights Reserved.
+ * This code is adapted from www.openscdp.org
+ * 
+ * Contributor(s):
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the EUPL.
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 3 or later (the "GPL")
+ * in which case the provisions of the GPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the GPL, and not to allow others to
+ * use your version of this file under the terms of the EUPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EUPL, or the GPL.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  EUPL for more details.
- *
- *  You should have received a copy of the EUPL along with this
- *  program.  If not, see:
- *  https://www.osor.eu/eupl, http://ec.europa.eu/idabc/eupl.
- *
- ************************************************************************/
+ * ***** END LICENSE BLOCK ******************************************** */
 
 /*
  * Original file header retained
@@ -55,6 +70,7 @@
  * File imported from http://www.openscdp.org/scsh3/download.html
  * 
  */
+
 package com.yacme.ext.oxsit.cust_it.security.crl;
 
 import java.io.InputStream;
