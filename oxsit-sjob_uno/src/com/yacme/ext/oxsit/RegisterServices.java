a654a900e95fba0c5f1803c5a5f7517ca97698cb
beppec56 <beppec56@ae2944e6-8456-422b-b884-7dc9435bd57f>
2011-07-15 13:29:41 +0000

    Updated license block
    git-svn-id: https://joinup.ec.europa.eu/svn/ooo-xadessig-it/trunk@625 ae2944e6-8456-422b-b884-7dc9435bd57f

--- oxsit-sjob_uno/src/com/yacme/ext/oxsit/RegisterServices.java
+++ oxsit-sjob_uno/src/com/yacme/ext/oxsit/RegisterServices.java
@@ -1,23 +1,40 @@
-/*************************************************************************
- *  Copyright 2009 by Giuseppe Castagno beppec56@openoffice.org
- *  
- *  The Contents of this file are made available subject to
- *  the terms of European Union Public License (EUPL) version 1.1
- *  as published by the European Community.
+/* ***** BEGIN LICENSE BLOCK ********************************************
+ * Version: EUPL 1.1/GPL 3.0
+ * 
+ * The contents of this file are subject to the EUPL, Version 1.1 or 
+ * - as soon they will be approved by the European Commission - 
+ * subsequent versions of the EUPL (the "Licence");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.osor.eu/eupl/european-union-public-licence-eupl-v.1.1
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the EUPL.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  EUPL for more details.
+ * The Original Code is oxsit-custom_it/src/com/yacme/ext/oxsit/cust_it/ConstantCustomIT.java.
  *
- *  You should have received a copy of the EUPL along with this
- *  program.  If not, see:
- *  https://www.osor.eu/eupl, http://ec.europa.eu/idabc/eupl.
+ * The Initial Developer of the Original Code is
+ * Giuseppe Castagno giuseppe.castagno@acca-esse.it
+ * 
+ * Portions created by the Initial Developer are Copyright (C) 2009-2011
+ * the Initial Developer. All Rights Reserved.
  *
- ************************************************************************/
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 3 or later (the "GPL")
+ * in which case the provisions of the GPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the GPL, and not to allow others to
+ * use your version of this file under the terms of the EUPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EUPL, or the GPL.
+ *
+ * ***** END LICENSE BLOCK ******************************************** */
 
 package com.yacme.ext.oxsit;
 
